a1a464ad92fe98813dd4660733e21cbdd907f5d2
Gabriel Jõe <77448069+DrigsterI@users.noreply.github.com>
2025-11-09 23:48:34 +0200

    add minimap settings draft

--- src/pages/map.rs
+++ src/pages/map.rs
@@ -3,7 +3,7 @@ use freya_radio::hooks::use_radio;
 
 use crate::{
     app::{Data, DataChannel},
-    components::DragableCanvas,
+    components::{Button, DragableCanvas, Setting, SettingType},
 };
 
 #[derive(PartialEq)]
@@ -32,70 +32,224 @@ impl Render for Map {
         let scale_y =
             (map_state.height as f64 - map_state.ocean_margin as f64 * 2.0) / (map_size as f64);
 
+        let mut minimap_page = use_state(|| false);
+        let mut minimap = use_state(|| false);
+
+        use_side_effect(move || {
+            if minimap() {
+                EventNotifier::get().launch_window(WindowConfig::new(move || {
+                    rect()
+                        .width(Size::Fill)
+                        .height(Size::Fill)
+                        .background(Color::BLACK)
+                        .child("I'm a minimap".to_string())
+                        .into()
+                }));
+            }
+        });
+
         rect()
-            .corner_radius(8.0)
-            .overflow_mode(OverflowMode::Clip)
             .width(Size::Fill)
             .height(Size::Fill)
-            .main_align(Alignment::Center)
-            .cross_align(Alignment::Center)
-            .background(
-                Color::from_hex(
-                    map_state
-                        .background
-                        .as_ref()
-                        .map(|s| s.as_str())
-                        .unwrap_or("#000000"),
-                )
-                .unwrap(),
-            )
-            .children([DragableCanvas::new()
-                .children([
+            .children([
+                rect()
+                    .width(Size::Fill)
+                    .height(Size::px(48.0))
+                    .padding(4.0)
+                    .spacing(4.0)
+                    .background(Color::BLACK)
+                    .direction(Direction::Horizontal)
+                    .children([
+                        Button::new()
+                            .width(Size::px(110.0))
+                            .height(Size::Fill)
+                            .align(Alignment::Center)
+                            .icon(freya_icons::lucide::map())
+                            .text("MAP")
+                            .on_press(move |_| {
+                                if minimap_page() {
+                                    minimap_page.set(false);
+                                }
+                            })
+                            .active(!minimap_page())
+                            .into(),
+                        Button::new()
+                            .height(Size::Fill)
+                            .icon(freya_icons::lucide::grid_2x2())
+                            .into(),
+                        Button::new()
+                            .height(Size::Fill)
+                            .icon(freya_icons::lucide::users_round())
+                            .into(),
+                        Button::new()
+                            .height(Size::Fill)
+                            .icon(freya_icons::lucide::skull())
+                            .into(),
+                        Button::new()
+                            .height(Size::Fill)
+                            .icon(freya_icons::lucide::factory())
+                            .into(),
+                        Button::new()
+                            .height(Size::Fill)
+                            .icon(freya_icons::lucide::store())
+                            .into(),
+                        rect().into(),
+                        Button::new()
+                            .width(Size::px(110.0))
+                            .height(Size::Fill)
+                            .align(Alignment::Center)
+                            .icon(freya_icons::lucide::locate_fixed())
+                            .text("MINIMAP")
+                            .on_press(move |_| {
+                                if !minimap_page() {
+                                    minimap_page.set(true);
+                                }
+                            })
+                            .active(minimap_page())
+                            .into(),
+                    ])
+                    .into(),
+                if minimap_page() {
                     rect()
-                        .width(Size::px(map_state.width as f32))
-                        .height(Size::px(map_state.height as f32))
-                        .child(ImageViewer::new(("map", image_bytes)))
-                        .into(),
-                    rect()
-                        .width(Size::px(map_state.width as f32))
-                        .height(Size::px(map_state.height as f32))
-                        .layer(1)
-                        .children(
-                            map_state
-                                .monuments
-                                .iter()
-                                .map(|monument| {
-                                    rect()
-                                        .width(Size::px(10.0))
-                                        .height(Size::px(10.0))
-                                        .corner_radius(CornerRadius::new_all(1000.0))
-                                        .background(Color::RED)
-                                        .position(
-                                            Position::new_absolute()
-                                                .left(
-                                                    (monument.x * scale_x as f32)
-                                                        + map_state.ocean_margin as f32,
-                                                )
-                                                .top(
-                                                    ((map_size as f32 - monument.y)
-                                                        * scale_y as f32)
-                                                        + map_state.ocean_margin as f32,
-                                                ),
-                                        )
-                                        .main_align(Alignment::Center)
-                                        .cross_align(Alignment::Center)
-                                        .children([label()
-                                            .width(Size::px(500.0))
-                                            .text_align(TextAlign::Center)
-                                            .text(monument.token.clone())
-                                            .into()])
-                                        .into()
+                        .width(Size::Fill)
+                        .height(Size::Fill)
+                        .padding(8.0)
+                        .spacing(4.0)
+                        .children([
+                            rect()
+                                .width(Size::Fill)
+                                .height(Size::px(48.0))
+                                .padding(8.0)
+                                .spacing(8.0)
+                                .direction(Direction::Horizontal)
+                                .cross_align(Alignment::Center)
+                                .children([
+                                    svg(freya_icons::lucide::map())
+                                        .height(Size::Fill)
+                                        .color(Color::from_hex("#5D7238").unwrap())
+                                        .into(),
+                                    label()
+                                        .font_size(24.0)
+                                        .font_weight(FontWeight::BOLD)
+                                        .color(Color::from_hex("#E4DAD1").unwrap())
+                                        .text("POSITION")
+                                        .into(),
+                                ])
+                                .into(),
+                            Setting::new(SettingType::Toggle)
+                                .text("ENABLED")
+                                .on_change(move |state| {
+                                    minimap.set(state);
                                 })
-                                .collect::<Vec<Element>>(),
+                                .into(),
+                            Setting::new(SettingType::Toggle).text("POSITION").into(),
+                            Setting::new(SettingType::Toggle).text("SIZE").into(),
+                            Setting::new(SettingType::Toggle).text("OFFSET").into(),
+                            rect()
+                                .width(Size::Fill)
+                                .height(Size::px(48.0))
+                                .padding(8.0)
+                                .spacing(8.0)
+                                .direction(Direction::Horizontal)
+                                .cross_align(Alignment::Center)
+                                .children([
+                                    svg(freya_icons::lucide::toggle_left())
+                                        .height(Size::Fill)
+                                        .color(Color::from_hex("#5D7238").unwrap())
+                                        .into(),
+                                    label()
+                                        .font_size(24.0)
+                                        .font_weight(FontWeight::BOLD)
+                                        .color(Color::from_hex("#E4DAD1").unwrap())
+                                        .text("TOGGLES")
+                                        .into(),
+                                ])
+                                .into(),
+                            Setting::new(SettingType::Toggle).text("GRID").into(),
+                            Setting::new(SettingType::Toggle).text("TEAMMATES").into(),
+                            Setting::new(SettingType::Toggle).text("MONUMENTS").into(),
+                            Setting::new(SettingType::Toggle).text("MARKERS").into(),
+                            Setting::new(SettingType::Toggle).text("DEATHS").into(),
+                        ])
+                        .into()
+                } else {
+                    rect()
+                        .padding(8.0)
+                        .child(
+                            rect()
+                                .corner_radius(8.0)
+                                .overflow_mode(OverflowMode::Clip)
+                                .width(Size::Fill)
+                                .height(Size::Fill)
+                                .main_align(Alignment::Center)
+                                .cross_align(Alignment::Center)
+                                .background(
+                                    Color::from_hex(
+                                        map_state
+                                            .background
+                                            .as_ref()
+                                            .map(|s| s.as_str())
+                                            .unwrap_or("#000000"),
+                                    )
+                                    .unwrap(),
+                                )
+                                .children([DragableCanvas::new()
+                                    .children([
+                                        rect()
+                                            .width(Size::px(map_state.width as f32))
+                                            .height(Size::px(map_state.height as f32))
+                                            .child(ImageViewer::new(("map", image_bytes)))
+                                            .into(),
+                                        rect()
+                                            .width(Size::px(map_state.width as f32))
+                                            .height(Size::px(map_state.height as f32))
+                                            .layer(1)
+                                            .children(
+                                                map_state
+                                                    .monuments
+                                                    .iter()
+                                                    .map(|monument| {
+                                                        rect()
+                                                            .width(Size::px(10.0))
+                                                            .height(Size::px(10.0))
+                                                            .corner_radius(CornerRadius::new_all(
+                                                                1000.0,
+                                                            ))
+                                                            .background(Color::RED)
+                                                            .position(
+                                                                Position::new_absolute()
+                                                                    .left(
+                                                                        (monument.x
+                                                                            * scale_x as f32)
+                                                                            + map_state.ocean_margin
+                                                                                as f32,
+                                                                    )
+                                                                    .top(
+                                                                        ((map_size as f32
+                                                                            - monument.y)
+                                                                            * scale_y as f32)
+                                                                            + map_state.ocean_margin
+                                                                                as f32,
+                                                                    ),
+                                                            )
+                                                            .main_align(Alignment::Center)
+                                                            .cross_align(Alignment::Center)
+                                                            .children([label()
+                                                                .width(Size::px(500.0))
+                                                                .text_align(TextAlign::Center)
+                                                                .text(monument.token.clone())
+                                                                .into()])
+                                                            .into()
+                                                    })
+                                                    .collect::<Vec<Element>>(),
+                                            )
+                                            .into(),
+                                    ])
+                                    .into()]),
                         )
-                        .into(),
-                ])
-                .into()])
+                        .into()
+                },
+            ])
             .into()
     }
 }
