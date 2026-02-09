bcfc936c887c24da087dd75e48c6949e232f9ff6
Gabriel Jõe <62962772+Drigster@users.noreply.github.com>
2026-02-09 20:53:14 +0200

    add dropdown setting

--- src/components/setting.rs
+++ src/components/setting.rs
@@ -3,8 +3,8 @@ use std::borrow::Cow;
 use freya::prelude::*;
 
 use crate::{
-    TEXT_COLOR,
-    components::{Button, Slider},
+    BACKGROUND_COLOR, INPUT_BACKGROUND, SELECT_COLOR, SIDEBAR_BUTTON_BACKGROUND, TEXT_COLOR,
+    components::{Button, Dropdown, Slider},
 };
 
 #[derive(PartialEq)]
@@ -23,11 +23,24 @@ pub struct SliderSettings {
 }
 
 #[derive(PartialEq)]
+pub struct DropdownSettings {
+    pub selected: String,
+    pub options: Vec<DropdownOption>,
+}
+
+#[derive(PartialEq)]
+pub struct DropdownOption {
+    pub name: String,
+    pub on_select: Option<EventHandler<()>>,
+    pub selected: Readable<bool>,
+}
+
+#[derive(PartialEq)]
 #[allow(dead_code)]
 pub enum SettingType {
     Toggle(ToggleSettings),
     Slider(SliderSettings),
-    Dropdown,
+    Dropdown(DropdownSettings),
 }
 
 #[derive(PartialEq)]
@@ -55,7 +68,7 @@ impl Component for Setting {
     fn render(&self) -> impl IntoElement {
         rect()
             .width(Size::Fill)
-            .height(Size::px(48.0))
+            .height(Size::px(56.0))
             .padding(8.0)
             .direction(Direction::Horizontal)
             .main_align(Alignment::SpaceBetween)
@@ -63,6 +76,7 @@ impl Component for Setting {
             .background(Color::from_hex("#0E0E0D80").unwrap())
             .children([
                 label()
+                    .margin(8.0)
                     .font_size(20.0)
                     .font_weight(FontWeight::BOLD)
                     .color(Color::from_hex(TEXT_COLOR).unwrap())
@@ -73,10 +87,11 @@ impl Component for Setting {
                         .width(Size::px(250.0))
                         .height(Size::Fill)
                         .align(Alignment::Center)
-                        .background(Color::from_hex("#0E0E0DBF").unwrap())
+                        .background(Color::from_hex(SIDEBAR_BUTTON_BACKGROUND).unwrap())
                         .background_hover(Color::from_hex("#0E0E0DBF").unwrap())
-                        .background_active(Color::from_hex("#434140").unwrap())
+                        .background_active(Color::from_hex(INPUT_BACKGROUND).unwrap())
                         .text(if settings.value { "ON" } else { "OFF" })
+                        .active(settings.value)
                         .on_press({
                             let on_change = settings.on_change.clone();
                             let value = settings.value;
@@ -102,9 +117,42 @@ impl Component for Setting {
                             }
                         })
                         .into(),
-                    SettingType::Dropdown => {
-                        rect().width(Size::px(250.0)).height(Size::Fill).into()
-                    }
+                    SettingType::Dropdown(dropdown_settings) => Dropdown::new()
+                        .width(Size::px(250.0))
+                        .height(Size::Fill)
+                        .font_size(16.0)
+                        .title(dropdown_settings.selected.clone())
+                        .background(Color::from_hex(SIDEBAR_BUTTON_BACKGROUND).unwrap())
+                        .background_chevron(Color::from_hex(INPUT_BACKGROUND).unwrap())
+                        .child(
+                            rect()
+                                .background(Color::from_hex("#0D0D0C").unwrap())
+                                .spacing(2.0)
+                                .padding(8.0)
+                                .children(dropdown_settings.options.iter().map(|option| {
+                                    Button::new()
+                                        .width(Size::Fill)
+                                        .height(Size::px(36.0))
+                                        .align(Alignment::Center)
+                                        .background(if *option.selected.read() {
+                                            Color::from_hex(SELECT_COLOR).unwrap()
+                                        } else {
+                                            Color::from_hex(BACKGROUND_COLOR).unwrap()
+                                        })
+                                        .text(option.name.clone())
+                                        .on_press({
+                                            let on_select = option.on_select.clone();
+                                            move |_| {
+                                                if let Some(on_select) = &on_select {
+                                                    on_select.call(());
+                                                }
+                                            }
+                                        })
+                                        .into()
+                                }))
+                                .into(),
+                        )
+                        .into(),
                 },
             ])
     }
